0b2de59e5a98896b4db8b72cced6d04bce96ed48
Unvoided <diogo.maxado02@gmail.com>
2025-05-24 19:41:59 +0100

    chore: re-added xlsx export

--- src/index.ts
+++ src/index.ts
@@ -4,92 +4,95 @@ import { antColony } from "./ant.ts";
 import { readTspFile } from "./loader.ts";
 import { scatterSearch } from "./scatter.ts";
 import { tabuSearch } from "./tabu.ts";
-
+import { saveResultsToXLSX } from "./xlsx.ts"
 /**
  * Files.
  */
 const fileNames = [
-  "a280",
-  "bier127",
-  "ch130",
-  "ch150",
-  "d198",
-  "eil101",
-  "gil262",
-  "kroA100",
-  "kroA150",
-  "kroA200",
-  "kroB100",
-  "kroB150",
-  "kroB200",
-  "kroC100",
-  "kroD100",
-  "kroE100",
-  "lin105",
-  "pr107",
-  "pr124",
-  "pr136",
-  "pr144",
-  "pr152",
-  "pr226",
-  "pr264",
-  "pr299",
-  "rat195",
-  "rd100",
-  "ts225",
-  "tsp225",
-  "u159",
+    "a280",
+    "bier127",
+    "ch130",
+    "ch150",
+    "d198",
+    "eil101",
+    "gil262",
+    "kroA100",
+    "kroA150",
+    "kroA200",
+    "kroB100",
+    "kroB150",
+    "kroB200",
+    "kroC100",
+    "kroD100",
+    "kroE100",
+    "lin105",
+    "pr107",
+    "pr124",
+    "pr136",
+    "pr144",
+    "pr152",
+    "pr226",
+    "pr264",
+    "pr299",
+    "rat195",
+    "rd100",
+    "ts225",
+    "tsp225",
+    "u159",
 ];
 const optimalFile = JSON.parse(
-  fs.readFileSync(path.join("./assets", "optimal.json"), "utf-8")
+    fs.readFileSync(path.join("./assets", "optimal.json"), "utf-8")
 );
 
 /**
  * Run algorithm.
  */
 async function runAlgorithm(
-  fileNames: string[],
-  algorithm: "ant" | "tabu" | "scatter",
-  params: number[]
+    fileNames: string[],
+    algorithm: "ant" | "tabu" | "scatter",
+    params: number[],
+    exportResults: boolean = false
 ) {
-  const results = [];
+    const results = [];
 
-  for (const fileName of fileNames) {
-    const file = await readTspFile(
-      path.join("./assets/problems", fileName.concat(".tsp"))
-    );
-    const optimal = optimalFile[fileName];
-    const algorithms = {
-      ant: () => antColony(file, ...params), // e.g., [1000, 40, 1, 10, 0.3, 500]
-      tabu: () => tabuSearch(file, ...params), // e.g., [100]
-      scatter: () => scatterSearch(file, ...params), // e.g., [100, 5, 30]
-    };
-    const result = algorithms[algorithm]();
+    for (const fileName of fileNames) {
+        const file = await readTspFile(
+            path.join("./assets/problems", fileName.concat(".tsp"))
+        );
+        const optimal = optimalFile[fileName];
+        const algorithms = {
+            ant: () => antColony(file, ...params), // e.g., [1000, 40, 1, 10, 0.3, 500]
+            tabu: () => tabuSearch(file, ...params), // e.g., [100]
+            scatter: () => scatterSearch(file, ...params), // e.g., [100, 5, 30]
+        };
+        const result = algorithms[algorithm]();
 
-    results.push({
-      file: file.name,
-      optimal: Math.round(optimal),
-      cost: Math.round(result.bestDistance),
-      time: (result.performance / 1000).toFixed(1).concat("s"),
-      deviation: (Math.abs((result.bestDistance - optimal) / optimal) * 100)
-        .toFixed(2)
-        .concat("%"),
-      params,
-    });
-  }
+        results.push({
+            file: file.name,
+            optimal: Math.round(optimal),
+            cost: Math.round(result.bestDistance),
+            time: (result.performance / 1000).toFixed(1).concat("s"),
+            deviation: (Math.abs((result.bestDistance - optimal) / optimal) * 100)
+                .toFixed(2)
+                .concat("%"),
+            params,
+        });
+    }
 
-  console.table(results);
+    console.table(results);
+
+    if (exportResults) saveResultsToXLSX(algorithm, results);
 }
 
 //runAlgorithm(fileNames, "ant", [1000, 40, 1, 10, 0.3, 500]);
-//runAlgorithm(["kroA100", "kroA150", "kroA200", "kroB100", "kroB150"], "tabu", [500]);
+runAlgorithm(fileNames, "tabu", [500], true);
 //runAlgorithm(
 //  ["pr107", "pr124", "pr136", "pr144", "pr152", "pr226", "pr264", "pr299"],
 //  "tabu",
 //  [500]
 //);
-runAlgorithm(
-  ["pr107", "pr124", "pr136", "pr144", "pr152", "pr226", "pr264", "pr299"],
-  "scatter",
-  [100, 5, 30]
-);
+// runAlgorithm(
+//     ["pr107", "pr124", "pr136", "pr144", "pr152", "pr226", "pr264", "pr299"],
+//     "scatter",
+//     [100, 5, 30]
+// );
--- src/xlsx.ts
+++ src/xlsx.ts
@@ -1,43 +1,55 @@
-/*
+
 import * as XLSX from "xlsx";
+import * as fs from "fs";
+import * as path from "path";
 
-const sheetData = [
-    [
-        "File",
-        "Optimal",
-        "Tabu Initial",
-        "Tabu Result",
-        "Tabu Time",
-        "Tabu Deviation",
-        "Scatter Initial",
-        "Scatter Result",
-        "Scatter Time",
-        "Scatter Deviation"
-    ],
-    ...results.map((r) => [
-        r.file,
-        r.optimal,
-        r.tabuInitial,
-        r.tabuResult,
-        r.tabuTime,
-        r.tabuDeviation,
-        r.scatterInitial,
-        r.scatterResult,
-        r.scatterTime,
-        r.scatterDeviation
-    ])
-];
-
-const workbook = XLSX.utils.book_new();
-const worksheet = XLSX.utils.aoa_to_sheet(sheetData);
-XLSX.utils.book_append_sheet(workbook, worksheet, "TSP Results");
-
-const outputDir = path.join(".", "output");
-if (!fs.existsSync(outputDir)) {
-    fs.mkdirSync(outputDir);
+interface Result {
+    file: string | undefined;
+    optimal: number;
+    cost: number;
+    time: string;
+    deviation: string;
+    params: number[];
 }
-const buffer = XLSX.write(workbook, { type: "buffer", bookType: "xlsx" });
-fs.writeFileSync(path.join(outputDir, "tsp_results.xlsx"), buffer);
 
-console.log("Results saved to output/tsp_results.xlsx");
-*/
+/**
+ * Saves the results of the TSP algorithms to an XLSX file.
+ * @param algorithm - The name of the algorithm used (e.g., "ant", "tabu", "scatter").
+ * @param results - An array of results containing file name, optimal cost, result cost, time, deviation, and parameters.
+ */
+export function saveResultsToXLSX(
+    algorithm: "ant" | "tabu" | "scatter",
+    results: Result[]
+) {
+    const sheetData = [
+        [
+            "File",
+            "Optimal",
+            "Result Cost",
+            "Time",
+            "Deviation",
+            "Parameters",
+        ],
+        ...results.map((r) => [
+            r.file,
+            r.optimal,
+            r.cost,
+            r.time,
+            r.deviation,
+            JSON.stringify(r.params)
+        ])
+    ];
+
+    const workbook = XLSX.utils.book_new();
+    const worksheet = XLSX.utils.aoa_to_sheet(sheetData);
+    XLSX.utils.book_append_sheet(workbook, worksheet, `${algorithm}_results`);
+
+    const outputDir = "assets/results";
+    if (!fs.existsSync(outputDir)) {
+        fs.mkdirSync(outputDir);
+    }
+    const buffer = XLSX.write(workbook, { type: "buffer", bookType: "xlsx" });
+    fs.writeFileSync(path.join(outputDir, `${algorithm}_results.xlsx`), buffer);
+
+    console.log(`Results saved to assets/results/${algorithm}_results.xlsx`);
+}
